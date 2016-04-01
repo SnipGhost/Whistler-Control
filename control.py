49cf139e367d00215009003057a21f003a8a7805
SnipGhost <snipghost@list.ru>
2016-04-01 13:57:55 +0300

    v3.1

--- control.py
+++ control.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
-
+#==========================================================
+# MOVEMENT CONTROL SCRIPT                              v3.1
+#==========================================================
 import sys
-
 from time import sleep
 from time import time
-
 import RPi.GPIO as GPIO
-
+#==========================================================
 GPIO.setmode(GPIO.BCM)
-
+#==========================================================
 if (len(sys.argv) == 4):
-
+    
     command = int(sys.argv[1])
-    
     value = float(sys.argv[2])
-    
     duc = int(sys.argv[3])
-    
+    #=======================
     GPIO.setwarnings(False)
-    
     #=======================
     GPIO.setup( 5, GPIO.OUT)
     pwm = GPIO.PWM(5, 15)
@@ -41,7 +38,7 @@ if (len(sys.argv) == 4):
     #-----------------------
     GPIO.output(20, False)
     #=======================
-		
+
     def PWMToggle():
         global pup
         if (pup != True):
@@ -54,7 +51,7 @@ if (len(sys.argv) == 4):
         else:
             pwm.stop()
             pup = False
-		
+
     def stop():
         GPIO.output( 4, GPIO.LOW)
         GPIO.output(27, GPIO.LOW)
@@ -81,9 +78,10 @@ if (len(sys.argv) == 4):
             pulse_duration = pulse_end - pulse_start
             distance = pulse_duration * 17150
             distance = round(distance, 2)
-            print distance,"\n cm"
+            print distance," cm\n"
             if (distance < 5+duc*0.2):
                 stop()
+                VT = 0
             if (VT > ST):
                 VT = VT - ST
                 sleep(ST)
@@ -105,7 +103,7 @@ if (len(sys.argv) == 4):
         print("FORWARD")
         TraceRoute(val)
         PWMToggle()
-    
+
     def backward(val):
         PWMToggle()
         GPIO.output( 4, GPIO.LOW)
@@ -168,4 +166,7 @@ if (len(sys.argv) == 4):
         print("Unknown command")
 
 else:
-    print("Usage: python control.py {0|1|2|3|4} SEC DC");
+    print("Usage: python control.py {0|1|2|3|4} SEC DC{0-100}");
+#==========================================================
+#                                              By SnipGhost
+#==========================================================
